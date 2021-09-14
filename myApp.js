376c814880c32d4ef07ef262f950c44319595230
xyalim <xyalim@outlook.com>
2021-09-14 18:15:24 +0800

    'init'

--- myApp.js
+++ myApp.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+const publicPath = __dirname + '/public'
 
+console.log("Hello World")
 
+app.get('/',function(req,res){
+  const htmlPath = __dirname + '/views/index.html'
+  res.sendFile(htmlPath)
+})
+app.use('/public',express.static(publicPath))
 
-
+app.get('/json',function (req,res) {
+  res.json({
+    message:'Hello json'
+  })
+})
 
 
 

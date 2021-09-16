278296bff96e023233299f5427e0da54d0c5a7c0
xyalim <xyalim@outlook.com>
2021-09-16 09:36:02 +0800

    save

--- myApp.js
+++ myApp.js
@@ -1,19 +1,45 @@
 var express = require('express');
+const dotenv = require('dotenv')
+dotenv.config('./env')
 var app = express();
 const publicPath = __dirname + '/public'
 
 console.log("Hello World")
 
-app.get('/',function(req,res){
+app.use("/", function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+
+app.get('/', function (req, res) {
   const htmlPath = __dirname + '/views/index.html'
   res.sendFile(htmlPath)
 })
-app.use('/public',express.static(publicPath))
+app.use('/public', express.static(publicPath))
 
-app.get('/json',function (req,res) {
+app.get('/json', function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({
+      message: 'HELLO JSON'
+    })
+  } else {
+    res.json({
+      message: 'Hello json'
+    })
+
+  }
+})
+app.use('/now',function (req, res, next) {
+  req.time = new Date().toString()
+  next()
+},)
+
+app.get('/now', function (req,res,next) {
   res.json({
-    message:'Hello json'
+    time: req.time
   })
+  next()
 })
 
 
@@ -45,6 +71,4 @@ app.get('/json',function (req,res) {
 
 
 
-
-
- module.exports = app;
+module.exports = app;
